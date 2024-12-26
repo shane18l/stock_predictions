fe981b13978399ad40cb7ee44187b3751fbc5e64
AwakeProgrammer <156217796+AwakeProgrammer@users.noreply.github.com>
2024-12-25 20:23:13 -0500

    Got stock data

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello")
+import yfinance as yf
+import pandas as pd
+
+
+data = yf.Ticker('AAPL')
+
+stock = data.history(period="max")
+print(stock)
+
+stock.plot.line(y="Close", use_index=True)
